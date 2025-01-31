e9b4459c929e3c9a68b7324152eb076c30934b82
roha-github <roha.mbox@googlemail.com>
2025-01-31 13:57:01 +0100

    Update coinseek-testlibrary-bit.py

--- coinseek-testlibrary-bit.py
+++ coinseek-testlibrary-bit.py
@@ -2,8 +2,14 @@ from hashlib import sha256
 from bit import Key
 from bit.format import bytes_to_wif
 from bit.format import public_key_to_segwit_address
+from bit.crypto import ripemd160_sha256
+from bit.base32 import encode
+import sys
 
 passwd = "testtesttesttest"
+if len(sys.argv) > 1:
+  passwd = " ".join(sys.argv[1:])
+
 entropy = sha256(passwd.encode()).hexdigest()
 key = Key.from_hex(entropy)
 wifu = bytes_to_wif(key.to_bytes(), compressed=False)
@@ -12,12 +18,15 @@ keyu = Key(wifu)
 caddr = key.address
 uaddr = keyu.address
 scadr = public_key_to_segwit_address(key._public_key)
+witprog = ripemd160_sha256(key._public_key)
+swadr = encode('bc', 0, witprog)
 
 # https://privatekeys.pw/key/5e8b64da785f1572e6da780648eaaffa009152d297bde80f852f068b0ec2989f
-print("Entropy Text                 :", passwd)  # testtesttesttest                                     (brainwallet)
-print("Entropy Hash                 :", entropy) # 5e8b64da785f1572e6da780648eaaffa009152d297bde80f852f068b0ec2989f
-print("Private Key compressed       :", wifc)    # KzPVYsYzN4KJLTHjqoD6i9h7UtfWQDUdMhg1f2gBrLxJnjv4Fm7b (C ~ WIF compressed)
-print("Private Key uncompressed     :", wifu)    # 5JXvbQgsc8Yncg2yqDHLRH1w2ngzQqbMUM6DPDALDzY7JCXUVLz  (U ~ WIF uncompressed) 
-print("Bitcoin Address compressed   :", caddr)   # 1DByDY2tA2CES5GYXsbsmfFX8VJMqP8frf                   (C ~ legacy compressed)
-print("Bitcoin Address uncompressed :", uaddr)   # 18JJvjw2XgwmTtVSv7HwKAJTEEFn7rT79P                   (U ~ legacy uncompressed)
-print("Bitcoin Address Script       :", scadr)   # 3MMpp9XvjDpFEiPhLaZSa4vmJVGmH2kXbG                   (S ~ Script / SegWit )
+print("Entropy Text                   :", passwd)  # testtesttesttest                                     (brainwallet)
+print("Entropy Hash                   :", entropy) # 5e8b64da785f1572e6da780648eaaffa009152d297bde80f852f068b0ec2989f
+print("Private Key compressed         :", wifc)    # KzPVYsYzN4KJLTHjqoD6i9h7UtfWQDUdMhg1f2gBrLxJnjv4Fm7b (C ~ WIF compressed)
+print("Private Key uncompressed       :", wifu)    # 5JXvbQgsc8Yncg2yqDHLRH1w2ngzQqbMUM6DPDALDzY7JCXUVLz  (U ~ WIF uncompressed) 
+print("Bitcoin Address C compressed   :", caddr)   # 1DByDY2tA2CES5GYXsbsmfFX8VJMqP8frf                   (C ~ legacy compressed)
+print("Bitcoin Address U uncompressed :", uaddr)   # 18JJvjw2XgwmTtVSv7HwKAJTEEFn7rT79P                   (U ~ legacy uncompressed)
+print("Bitcoin Address S Script       :", scadr)   # 3MMpp9XvjDpFEiPhLaZSa4vmJVGmH2kXbG                   (S ~ Script / SegWit )
+print("Bitcoin Address W SegWit       :", swadr)   # bc1qsk6f3zmc4rp4cjylag7zw2zknhkezxk8zp87ru           (W ~ SegWit / Bench32 )
